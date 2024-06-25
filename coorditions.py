3a8ebf51c95e5094c9b0cb29a92a3b65fd6f4ded
Stanislav Sorochenko <81685498+Staks-sor@users.noreply.github.com>
2024-06-25 15:07:58 +0300

    Update coorditions.py
    Небольшие изменения

--- coorditions.py
+++ coorditions.py
@@ -2,36 +2,39 @@ import pyautogui
 import keyboard
 
 
-def select_area():
-    print("Выберите область на экране.")
-    print("Нажмите F5, чтобы начать выделение.")
-    print("Нажмите F6, чтобы закончить выделение.")
+class AreaSelector:
+    def __init__(self):
+        self.start_x, self.start_y = None, None
+        self.end_x, self.end_y = None, None
 
-    start_x, start_y = None, None
-    end_x, end_y = None, None
+    def select_area(self):
+        print("Выберите область на экране.")
+        print("Нажмите F5, чтобы начать выделение.")
+        print("Нажмите F6, чтобы закончить выделение.")
 
-    # Ожидание нажатия клавиши F5 для начала выделения
-    keyboard.wait("F5")
+        # Ожидание нажатия клавиши F5 для начала выделения
+        keyboard.wait("F5")
 
-    # Начало выделения: получение координат начальной точки
-    start_x, start_y = pyautogui.position()
-    end_x, end_y = start_x, start_y
+        # Начало выделения: получение координат начальной точки
+        self.start_x, self.start_y = pyautogui.position()
+        self.end_x, self.end_y = self.start_x, self.start_y
 
-    # Ожидание нажатия клавиши F6 для завершения выделения
-    keyboard.wait("F6")
+        # Ожидание нажатия клавиши F6 для завершения выделения
+        keyboard.wait("F6")
 
-    # Завершение выделения: получение координат конечной точки
-    end_x, end_y = pyautogui.position()
+        # Завершение выделения: получение координат конечной точки
+        self.end_x, self.end_y = pyautogui.position()
 
-    # Вычисление координат и размеров прямоугольной области
-    left = min(start_x, end_x)
-    top = min(start_y, end_y)
-    width = abs(end_x - start_x)
-    height = abs(end_y - start_y)
+        # Вычисление координат и размеров прямоугольной области
+        left = min(self.start_x, self.end_x)
+        top = min(self.start_y, self.end_y)
+        width = abs(self.end_x - self.start_x)
+        height = abs(self.end_y - self.start_y)
 
-    return left, top, width, height
+        return left, top, width, height
 
 
 if __name__ == '__main__':
-    selected_area = select_area()
+    selector = AreaSelector()
+    selected_area = selector.select_area()
     print("Координаты выделенной области:", selected_area)
